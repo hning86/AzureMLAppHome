c8bca695ec051f36670b7fa919eb6022d0159cc7
hning86 <haining@microsoft.com>
2017-10-30 14:51:32 -0400

    broken e13n link

---
+++ js/versions.js
@@ -0,0 +1,4 @@
+var latestReleaseVersion = "0.1.1710.04013";
+var latestReleaseNotes = "<a href=\"https://docs.microsoft.com/azure/machine-learning/preview/release-notes-sprint-0\">October 2017 Update</a> (" + latestReleaseVersion + ")";
+
+var nextReleaseNotes = "https://aka.ms/azureml-nextreleasenotes";
